37b63aeca397f10e16432402d05696262f2bbf8e
sensational.mack <mackn478@gmail.com>
2022-11-30 13:25:08 -0500

    add seeds-lessondata.json-to connect with mongodb

--- server/models/Lessons.js
+++ server/models/Lessons.js
@@ -3,13 +3,20 @@ const {Schema, model} = require("mongoose");
 const lessonSchema = new Schema ({
     startingmoves: {
         type: String,
-        required: true,
+        // required: true,
     },
 
     lessontitle: {
         type: String,
-        required: true,
+        // required: true,
     },
+
+    
+        lessondescription: {
+            type: String,
+            // required: true,
+        }
+    
 })
 
 const Lessons = model('Lessons', lessonSchema)
