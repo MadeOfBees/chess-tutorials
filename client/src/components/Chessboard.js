dc95ed021e2ca53c4ed43c1d1b7dc1d27e1bcf86
Bee <xronsredstone@gmail.com>
2022-11-30 13:37:28 -0500

    fixed bug

--- client/src/components/Chessboard.js
+++ client/src/components/Chessboard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Chessdiagram from 'react-chessdiagram';
 
-const lightSquareColor = '#a8edbd';
-const darkSquareColor = '#5d4d6b';
+
+const lightSquareColor = '#FFFFFF';
+const darkSquareColor = '#999999';
 const flip = false;
 const squareSize = 40;
 
@@ -10,14 +11,15 @@ function onMovePiece(piece, fromSquare, toSquare) {
     let message = 'You moved ' + piece + fromSquare + ' to ' + toSquare + ' !';
     console.log(message);
 }
+
 function Chessboard(currentPosition) {
     // DEMO, PLEASE DELETE LATER
-    const samplePositionChangeToCurrentPosition =  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+    const samplePosition =  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
     // DEMO, PLEASE DELETE LATER
     return (
         <div>
-            <Chessdiagram flip={flip} fen={samplePositionChangeToCurrentPosition} squareSize={squareSize}
-                lightSquareColor={lightSquareColor} darkSquareColor={darkSquareColor} onMovePiece={onMovePiece} />,
+            <Chessdiagram flip={flip} fen={samplePosition} squareSize={squareSize}
+                lightSquareColor={lightSquareColor} darkSquareColor={darkSquareColor} onMovePiece={onMovePiece} />
         </div>
     );
 }
