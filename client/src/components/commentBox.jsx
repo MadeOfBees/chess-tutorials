dd7af9fa5aa73bd4fc92e8110f2164c18206636e
arjun gupta <arjunkarangupta100@gmail.com>
2022-11-30 21:25:04 -0500

    fixed that stupid bug that didnt let me include the use ref and use state hooks if the function that I needed to refrence them to isnt a react function because the name wasnt capitalized which I am already so against when the first letter is capitol for the names  of variables and classes and stuff\

--- client/src/components/commentBox.jsx
+++ client/src/components/commentBox.jsx
@@ -1,11 +1,10 @@
-
 import React, { useState, useRef } from "react";
 import cn from "classnames";
 import "../commentStyles.css";
 
 const START_HEIGHT = 40
 
-const commentBox = () => {
+const CommentBox = () => {
 
     const [isExpanded, setIsExpanded] = useState(false);
     const [commentValue, setCommentValue] = useState("");
@@ -20,24 +19,30 @@ const commentBox = () => {
         if(!isExpanded) {
             outerHeight.current = containerRef.current.scrollHeight;
             setIsExpanded(true);
-        }
-    }
+        };
+    };
     //will change comment value depending on the amount of comments
     const onChange = (e) =>{
         setCommentValue(e.target.value);
     }
+
     //sets comment value to zero when the box is closed
     const onClose = () => {
         setCommentValue("");
         setIsExpanded(false);
     }
+
+
+
+
     //stops from automatically clearing the data
     const onSubmit = (e) => {
         e.preventDefault();
         console.log('sending form data somewhere I guess')
     }
-    //add a form to the return
-    return (<form
+    //return the forma data and look by...
+    return (
+    <form
     onSubmit={onSubmit}
     ref={containerRef}
     className={cn("comment-box", {
@@ -56,7 +61,7 @@ const commentBox = () => {
 
     <label htmlFor="comment">Add your comment/tip/frustration Here</label>
 
-
+    
     <textarea 
     ref={textRef}
     onClick={onExpand}
@@ -69,8 +74,14 @@ const commentBox = () => {
     id="comment"
     />
 
+    <div className="responseButtons">
+        <button type="button" className="nevermindButton" onClick={onClose}></button>
+        <button type="submit" disabled={commentValue.length < 1}></button>
+
+    </div>
+
     </form>);
 
 };
 
-export default commentBox;
+export default CommentBox;
