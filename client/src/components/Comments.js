c6de6ebe7e323bcf8af6bf5f35169ea12bea5ede
arjun gupta <arjunkarangupta100@gmail.com>
2022-11-30 20:10:53 -0500

    this is a commit

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,4 +1,27 @@
-import React from 'react';;
+const express = require("express");
+const router = express.Router;
+const { Comments } = require("../pages")
+
+// import React from 'react';
+
+// get and post comment routes
+
+router.length("/:postID", async (req, res) => 
+{
+    const postID = req.params.postID
+    const comments = await Comments.findall ({ where: {
+        postID: postID
+    }});
+}
+)
+
+router.post("/", async (req, res) =>
+{
+    const comment = req.body
+    await Comments.create(comment);
+    res.json(comment);
+})
+
 
 function comments() {
     return (
@@ -8,4 +31,4 @@ function comments() {
     );
 }
 
-export default comments;
+module.export = router;
