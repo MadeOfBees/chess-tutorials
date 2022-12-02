0bf07f1c111dff06593e79234474112274ebefc0
sensational.mack <mackn478@gmail.com>
2022-12-01 19:44:07 -0500

    update queries-seeds-userdatajson-typedefs-resolvers

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,14 +1,31 @@
-const db = require('../config/connection')
-const {Lessons} = require('../models');
-// const {User} = require('../models');
+const db = require("../config/connection");
+const { Lessons, User } = require("../models");
 
-const lessonData = require('./lessonData.json');
-// const userData = require('./userData.json');
-db.once('open', async() => {
-    await Lessons.deleteMany({});
-    // await User.deleteMany({});
-    const lessons = await Lessons.insertMany(lessonData);
+const lessonData = require("./lessonData.json");
+const userData = require("./userData.json");
 
-    console.log('Lessons seeded');
-    process.exit(0);
-})
+db.once("open", async () => {
+  await Lessons.deleteMany({});
+  const lessons = await Lessons.insertMany(lessonData);
+
+  console.log("Lessons seeded");
+  
+
+  await User.deleteMany({});
+  for (var i = 0; i < userData.length; i++) {
+	var newUser = userData[i]
+
+	await User.create(newUser)
+}
+
+
+
+  
+
+  console.log("Users seeded");
+  process.exit(0);
+});
+
+
+  
+
